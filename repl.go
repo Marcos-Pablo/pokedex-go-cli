868c7a946b11bda25dbffa8f6f7eab485838a913
Marcos Pablo <marquin.pablo@gmail.com>
2026-02-01 16:19:54 -0300

    feat(repl): implement basic REPL loop
    Add a simple REPL loop that reads user input, cleans it, and prints the
    command. Update .gitignore to exclude repl.log. Refactor main.go to start
    the REPL instead of printing a static message.

--- repl.go
+++ repl.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		cmd := words[0]
+		fmt.Printf("Your command was: %s\n", cmd)
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
